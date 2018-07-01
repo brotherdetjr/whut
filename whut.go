92749dcf5fc523c899b0dd1034299ad44c330840
Denis Karpov <brotherdetjr@mail.ru>
2018-07-02 00:42:35 +0100

    initial commit

---
+++ whut.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+    "fmt"
+    "os"
+    "net/http"
+    "encoding/json"
+    "jaytaylor.com/html2text"
+)
+
+func main() {
+    if len(os.Args) < 2 {
+        fmt.Println("Usage: whut <word to translate>")
+        os.Exit(1)
+    }
+    url := "https://glosbe.com/gapi/translate?from=eng&dest=rus&format=json&phrase=" + os.Args[1]
+    client := &http.Client{}
+    entry := &Entry{}
+    getJson(client, url, entry)
+    for idx, val := range toSlice(entry) {
+        fmt.Printf("%d. %s\n", idx + 1, val)
+    }
+}
+
+type Entry struct {
+    Tuc []Option `json:"tuc"`
+}
+
+type Option struct {
+    Phrase Value `json:"phrase"`
+    Meanings []Value `json:"meanings"`
+}
+
+type Value struct {
+    Text string `json:"text"`
+    Language string `json:"language"`
+}
+
+func getJson(client *http.Client, url string, target interface{}) error {
+    r, err := (*client).Get(url)
+    if err != nil {
+        panic(err)
+    }
+    defer r.Body.Close()
+    return json.NewDecoder(r.Body).Decode(target)
+}
+
+func toSlice(entry *Entry) []string {
+    var result []string
+    for _, val := range (*entry).Tuc {
+        if (val.Phrase != Value{}) {
+            result = appendText(result, val.Phrase.Text)
+        } else {
+            for _, val := range val.Meanings {
+                result = appendText(result, val.Text)
+            }
+        }
+    }
+    return result
+}
+
+func appendText(result []string, text string) []string {
+    txt, err := html2text.FromString(text, html2text.Options{})
+    if err == nil {
+        return append(result, txt)
+    } else {
+        return result
+    }
+}
---
+++ whut_test.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+    "testing"
+    "encoding/json"
+    "reflect"
+)
+
+func TestJsonUnmarshal(t *testing.T) {
+    t.Parallel()
+    jsonStr := `
+{
+  "result" : "ok",
+  "tuc" : [ {
+    "phrase" : {
+      "text" : "песочное печенье",
+      "language" : "ru"
+    },
+    "meaningId" : -638808362965036511,
+    "authors" : [ 2899 ]
+  }, {
+    "meanings" : [ {
+      "language" : "en",
+      "text" : "very rich thick butter cookie"
+    } ],
+    "meaningId" : null,
+    "authors" : [ 93369 ]
+  }, {
+    "meanings" : [ {
+      "language" : "en",
+      "text" : "blah blah blah"
+    } ],
+    "meaningId" : null,
+    "authors" : [ 1 ]
+  } ],
+  "phrase" : "shortbread",
+  "from" : "en",
+  "dest" : "ru",
+  "authors" : {
+    "1" : {
+      "U" : "http://en.wiktionary.org",
+      "id" : 1,
+      "N" : "en.wiktionary.org",
+      "url" : "https://glosbe.com/source/1"
+    },
+    "2899" : {
+      "U" : "http://sourceforge.net/projects/freedict/files/",
+      "id" : 2899,
+      "N" : "English-Russian",
+      "url" : "https://glosbe.com/source/2899"
+    },
+    "93369" : {
+      "U" : "http://plwordnet.pwr.wroc.pl/wordnet/",
+      "id" : 93369,
+      "N" : "plwordnet-defs",
+      "url" : "https://glosbe.com/source/93369"
+    }
+  }
+}
+    `
+    var actual Entry
+    err := json.Unmarshal([]byte(jsonStr), &actual)
+    if err != nil {
+        t.Fail()
+    }
+    expected := Entry{
+        []Option{
+            Option{Phrase: Value{"песочное печенье", "ru"}},
+            Option{Meanings: []Value{Value{"very rich thick butter cookie", "en"}}},
+            Option{Meanings: []Value{Value{"blah blah blah", "en"}}},
+        },
+    }
+    if !reflect.DeepEqual(actual, expected) {
+        t.Fail()
+    }
+}
+
+func TestJsonUnmarshalEmpty(t *testing.T) {
+    t.Parallel()
+    jsonStr := `
+{
+  "result" : "ok",
+  "tuc" : [ ],
+  "phrase" : "idontexist",
+  "from" : "en",
+  "dest" : "ru",
+  "authors" : { }
+}
+    `
+    var actual Entry
+    err := json.Unmarshal([]byte(jsonStr), &actual)
+    if err != nil {
+        t.Fail()
+    }
+    if !reflect.DeepEqual(actual, Entry{[]Option{}}) {
+        t.Fail()
+    }
+}
+
+func TestToSlice(t *testing.T) {
+    t.Parallel()
+    parsed := Entry{
+        []Option{
+            Option{Phrase: Value{"песочное печенье", "ru"}},
+            Option{
+                Meanings: []Value{
+                    Value{"very rich <b>thick</b> butter cookie", "en"},
+                    Value{"&quot;blah&quot;", "fr"},
+                },
+            },
+            Option{Meanings: []Value{Value{"blah blah blah&#39;s", "en"}}},
+        },
+    }
+    expected := []string{"песочное печенье", "very rich *thick* butter cookie", `"blah"`, "blah blah blah's"}
+    if !reflect.DeepEqual(toSlice(&parsed), expected) {
+        t.Fail()
+    }
+}
+
+func TestToSliceEmpty(t *testing.T) {
+    t.Parallel()
+    if toSlice(&Entry{[]Option{}}) != nil {
+        t.Fail()
+    }
+}
